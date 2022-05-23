a3c401164acd41ecfee947af3556f9f30cd9319e
hirata423 <ani335@icloud.com>
2022-05-23 15:52:59 +0900

    Vercelでデプロイ-公開作業

--- next.config.js
+++ next.config.js
@@ -1,16 +1,16 @@
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//   reactStrictMode: true,
-// };
-
-// module.exports = {
-//  nextConfig
-// };
-
-const urlPrefix = process.env.URL_PREFIX ? "/" + process.env.URL_PREFIX : "";
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+};
 
 module.exports = {
-  assetPrefix: urlPrefix,
-  basePath: urlPrefix,
-  trailingSlash: true,
+  nextConfig,
 };
+
+// const urlPrefix = process.env.URL_PREFIX ? "/" + process.env.URL_PREFIX : "";
+
+// module.exports = {
+//   assetPrefix: urlPrefix,
+//   basePath: urlPrefix,
+//   trailingSlash: true,
+// };
